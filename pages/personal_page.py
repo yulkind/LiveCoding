0c4892029500a05ca8c32c7379c22bfff9489477
Yulia Glyvina <glyvinajulia@yandex.ru>
2024-09-13 15:28:51 +0300

    Finish of CI/CD

--- pages/personal_page.py
+++ pages/personal_page.py
@@ -1,5 +1,3 @@
-import time
-
 import allure
 from base.base_page import BasePage
 from config.links import Links
--- tests/feature_profile_test.py
+++ tests/feature_profile_test.py
@@ -1,17 +1,13 @@
 import random
-import time
-
 import allure
 import pytest
-
 from base.base_test import BaseTest
 
-
-@allure.feature('Profile Functionality')
+@allure.feature("Profile Functionality")
 class TestProfileFeature(BaseTest):
 
-    @allure.title('Change profile name')
-    @allure.severity('Critical')
+    @allure.title("Change profile name")
+    @allure.severity("Critical")
     @pytest.mark.smoke
     def test_change_profile_name(self):
         self.login_page.open()
@@ -21,8 +17,8 @@ class TestProfileFeature(BaseTest):
         self.dashboard_page.is_opened()
         self.dashboard_page.click_my_info_link()
         self.personal_page.is_opened()
-        self.personal_page.change_name(f'Test {random.randint(1, 100)}')
-        time.sleep(6)
+        self.personal_page.change_name(f"Test {random.randint(1, 100)}")
         self.personal_page.save_changes()
         self.personal_page.is_changes_saved()
         self.personal_page.make_screenshot("Success")
+
